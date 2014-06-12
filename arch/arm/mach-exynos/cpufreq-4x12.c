c2e2ed23e36c95ef019c802225cd594030470fc9
Allen Zhong <moeallenz@gmail.com>
2014-06-12 22:54:28 +0800

    Minor fix of ABB

--- arch/arm/mach-exynos/cpufreq-4x12.c
+++ arch/arm/mach-exynos/cpufreq-4x12.c
@@ -496,11 +496,19 @@ static void exynos4x12_set_frequency(unsigned int old_index,
 				need_dynamic_ema)
 				__raw_writel(0x101, EXYNOS4_EMA_CONF);
 
-		if ((samsung_rev() >= EXYNOS4412_REV_2_0)
+#ifndef CONFIG_ABB_CONTROL
+		if ((soc_is_exynos4212())
+			&& (exynos_result_of_asv > 3)
+			&& (old_index > L7) && (new_index <= L7)) {
+			exynos4x12_set_abb_member(ABB_ARM, ABB_MODE_130V);
+		} else if ((samsung_rev() >= EXYNOS4412_REV_2_0)
 			&& (exynos_result_of_asv > 2)
 			&& (old_index > L8) && (new_index <= L8)) {
 			exynos4x12_set_abb_member(ABB_ARM, ABB_MODE_130V);
 		}
+#else
+		abb_target(ABB_ARM, exynos4x12_freq_table[new_index].frequency);
+#endif
 
 		if (!exynos4x12_pms_change(old_index, new_index)) {
 			/* 1. Change the system clock divider values */
@@ -534,16 +542,25 @@ static void exynos4x12_set_frequency(unsigned int old_index,
 			/* 2. Change the system clock divider values */
 			set_clkdiv(new_index);
 		}
-		if ((samsung_rev() >= EXYNOS4412_REV_2_0)
+#ifndef CONFIG_ABB_CONTROL
+		if ((soc_is_exynos4212())
+			&& (exynos_result_of_asv > 3)
+			&& (old_index <= L7) && (new_index > L7)) {
+			exynos4x12_set_abb_member(ABB_ARM, ABB_MODE_100V);
+		} else if ((samsung_rev() >= EXYNOS4412_REV_2_0)
 			&& (exynos_result_of_asv > 2)
 			&& (old_index <= L8) && (new_index > L8)) {
 			exynos4x12_set_abb_member(ABB_ARM, ABB_MODE_100V);
 		}
+#else
+		abb_target(ABB_ARM, exynos4x12_freq_table[new_index].frequency);
+#endif
 		if (exynos4x12_volt_table[new_index] < 950000 &&
 				need_dynamic_ema)
 			__raw_writel(0x404, EXYNOS4_EMA_CONF);
 	}
 
+#ifndef CONFIG_ABB_CONTROL
 	/* ABB value is changed in below case */
 	if (soc_is_exynos4412() && (exynos_result_of_asv > 3)) {
 		if (new_index == L16)
@@ -551,6 +568,7 @@ static void exynos4x12_set_frequency(unsigned int old_index,
 		else
 			exynos4x12_set_abb_member(ABB_ARM, ABB_MODE_130V);
 	}
+#endif
 }
 
 /* Get maximum cpufreq index of chip */
