bca1e17a1c6d60c6c750490ed7b69561af897c93
Allen Zhong <moeallenz@gmail.com>
2014-07-17 14:37:31 +0800

    Revert "smdk4412: update mali driver from GT-N7100_SEA_KK_Opensource"
    This reverts commit 4978fd6d0886d3a0e08637376d46b5065f15e3dc.
    
    Conflicts:
    	drivers/gpu/mali400/r3p2/mali/common/mali_mem_validation.c

--- drivers/gpu/mali400/r3p2/mali/common/mali_mem_validation.c
+++ drivers/gpu/mali400/r3p2/mali/common/mali_mem_validation.c
@@ -11,15 +11,6 @@
 #include "mali_mem_validation.h"
 #include "mali_osk.h"
 #include "mali_kernel_common.h"
-/* MALI_SEC */
-#if defined(CONFIG_CPU_EXYNOS4212) || defined(CONFIG_CPU_EXYNOS4412)
-#ifndef CONFIG_MACH_KONA
-#define MALI_SEC_MEM_VALIDATION
-#endif
-#include <linux/cma.h>
-#include <plat/pd.h>
-#include <linux/platform_device.h>
-#endif
 
 #define MALI_INVALID_MEM_ADDR 0xFFFFFFFF
 
